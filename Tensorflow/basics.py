2e6482cf8c32e5141a14d72c013e75d419537b49
Floris Nzabakira <nzafloris@gmail.com>
2023-10-13 08:13:07 +0200

    starting basics

--- Tensorflow/basics.py
+++ Tensorflow/basics.py
@@ -1,5 +1,32 @@
-import tensorflow as tf
+"""
+Definitions:
+    tensor: multi dimensional array that can be run on GPU
+
+"""
+
 import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = '2'
 
+import tensorflow as tf
+import numpy as np
 
-print(tf.__version__)
+# initialization of tensors
+# constants
+x = tf.constant(4, shape=(1,1), dtype=tf.float32)
+x_2d = tf.constant([[1,2,3],[1,2,3]]) 
+
+ones = tf.ones((3,3))
+
+zeros = tf.zeros((2,3))
+
+identical_matrix = tf.eye(3)
+
+random_normal = tf.random.normal((3,3), mean=0, stddev=1)
+
+random_uniform = tf.random.uniform((4,3), minval=0, maxval=1)
+
+range_n = tf.range(start=1, 
+                   limit=10, 
+                   delta=2 # skip 
+                   )
+
